ef75146ab249766fc80fda2511c130d01a7c2a70
Natiq Mumtaz <natiqkhan00@gmail.com>
2021-07-06 15:42:12 +0200

    feat: wip navbar and add jsconfig

---
+++ components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className="logo">n</div>
+            <div>
+                <ul>
+                    <li>about</li>
+                    <li>mems</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
